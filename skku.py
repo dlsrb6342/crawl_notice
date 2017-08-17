3baf714c337caf2613e3a65fcded537a13822bfa
dlsrb6342 <dlsrb6342@gmail.com>
2017-08-17 16:10:29 +0900

    Update skku notice crawling

--- skku.py
+++ skku.py
@@ -5,11 +5,11 @@ from datetime import datetime
 
 def get_skku_notice(last_num, logger):
     result = []
-    
-    while True:
-        find_num = last_num + 1
+    num_list = get_notice_num_list(last_num)[::-1]
+    for i in num_list:
+        print(i)
         URL = "http://www.skku.edu/new_home/campus/skk_comm/notice_view.jsp?bCode=0&page=1&boardNum="
-        URL = URL + str(find_num) + "&virtualNum=0&skey=BOARD_SUBJECT&keyword=&bName=board_news&bCode=0"
+        URL = URL + str(i) + "&virtualNum=0&skey=BOARD_SUBJECT&keyword=&bName=board_news&bCode=0"
         response = requests.get(URL)
         if response.status_code != requests.codes.ok:
             break
@@ -17,7 +17,6 @@ def get_skku_notice(last_num, logger):
             soup = BeautifulSoup(response.text, 'html5lib')
             title = soup.td.extract().text
             soup.td.extract()
-            last_num = int(soup.td.extract().text)
             soup.td.extract()
             soup.td.extract()
             contents = soup.find(id="contents").div.text.strip()
@@ -38,7 +37,7 @@ def get_skku_notice(last_num, logger):
                             img_src = ""
             notice = {
                 'title': title,
-                'last_num': last_num,
+                'last_num': i,
                 'contents': contents,
                 'link': URL,
                 'img_src': img_src
@@ -47,5 +46,26 @@ def get_skku_notice(last_num, logger):
         except:
             break
 
-    logger.info('get '+ str(len(result)) + ' new skku notice')
+    #logger.info('get '+ str(len(result)) + ' new skku notice')
     return result
+
+
+def get_notice_num_list(last_num): 
+    page = 1
+    num_list = []
+    index_list = [ 5 * x for x in range(0, 10) ]
+    while True:
+        URL = 'http://www.skku.edu/new_home/campus/skk_comm/notice_list.jsp?bName=board_news&bCode=0&page=' + str(page)
+        response = requests.get(URL)
+        if response.status_code != requests.codes.ok:
+            return num_list
+        try:
+            soup = BeautifulSoup(response.text, 'html5lib')
+            notice_list = soup.findAll('td')
+            for i in index_list:
+                if notice_list[i].text == str(last_num):
+                    return num_list
+                num_list.append(notice_list[i].text)
+            page += 1
+        except:
+            return num_list
